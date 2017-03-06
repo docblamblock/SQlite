4588e718227c930e063109477af3cf1dab096b4c
docblamblock <darrenconnors@hotmail.com>
2017-03-06 17:39:07 +0000

    Add files via upload

--- www/js/index_rearranged.js
+++ www/js/index_rearranged.js
@@ -236,9 +236,14 @@ $(document).ready(function() {
       
                 //Display Table Data
           $("#showTable").click(function(){
+          
+          
+          
             $("#TableData").html("");
             myDB.transaction(function(transaction) {
-            transaction.executeSql('SELECT * FROM phonegap_pro', [], function (tx, results) {
+            
+            var kinlay = "kinlay";
+            transaction.executeSql('SELECT * FROM phonegap_pro where title LIKE ?', [kinlay], function (tx, results) {
             
              
             
@@ -250,12 +255,13 @@ $(document).ready(function() {
                  $("#rowCount").html(len);
                  for (i = 0; i < len; i++){
                  
+                 /*
                  var image = new Image();
                  image.src = results.rows.item(i).desc;
                  image.width = 50;
-                 
+                  */
                  $("#TableData").append("<tr><td>"); 
-                 $("#TableData").append(image);
+                 //$("#TableData").append(image);
                  $("#TableData").append("</td><td>"+results.rows.item(i).title+"</td><td>Click here</td></tr>"); 
                  
                     //$("#TableData").append(image+"<tr><td>"+results.rows.item(i).id+"</td><td>"+results.rows.item(i).title+"</td><td>"+results.rows.item(i).desc+"</td><td><a href='edit.html?id="+results.rows.item(i).id+"&title="+results.rows.item(i).title+"&desc="+results.rows.item(i).desc+"'>Edit</a> &nbsp;&nbsp; <a class='delete' href='#' id='"+results.rows.item(i).id+"'>Delete</a></td></tr>");
