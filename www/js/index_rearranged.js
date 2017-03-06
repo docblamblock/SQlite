a946ae6d7802a5cdd762b781e89c1e7a49cce575
docblamblock <darrenconnors@hotmail.com>
2017-03-06 16:55:58 +0000

    Add files via upload

--- www/js/index_rearranged.js
+++ www/js/index_rearranged.js
@@ -256,7 +256,7 @@ $(document).ready(function() {
                  
                  $("#TableData").append("<tr><td>"); 
                  $("#TableData").append(image);
-                 $("#TableData").append("<br>"+results.rows.item(i).title+"</td></tr>"); 
+                 $("#TableData").append("</td><td>"+results.rows.item(i).title+"</td><td>Click here</td></tr>"); 
                  
                     //$("#TableData").append(image+"<tr><td>"+results.rows.item(i).id+"</td><td>"+results.rows.item(i).title+"</td><td>"+results.rows.item(i).desc+"</td><td><a href='edit.html?id="+results.rows.item(i).id+"&title="+results.rows.item(i).title+"&desc="+results.rows.item(i).desc+"'>Edit</a> &nbsp;&nbsp; <a class='delete' href='#' id='"+results.rows.item(i).id+"'>Delete</a></td></tr>");
                     
